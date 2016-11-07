f6432ceb163f7f7196fc305cb1b10052a4da31c4
Natasjah <nhelle14@student.aau.dk>
2016-11-07 15:21:29 +0100

    added conveyor belt animation, office shelf and storage shelves

---
+++ LearningProgrammingGameUnity/Assets/Resources/GameObjects/conveyorAnimatorScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class conveyorAnimatorScript : MonoBehaviour {
+
+	Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (true)
+			animator.SetTrigger ("on");
+		if (false)
+			animator.SetTrigger ("off");
+	
+	}
+}
